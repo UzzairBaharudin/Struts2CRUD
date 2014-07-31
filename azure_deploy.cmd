84caad4144014e7231e6cf96b200a33669e4bde8
Chen Yu Pao <windperson@gmail.com>
2014-08-01 03:52:30 +0800

    debug

--- azure_deploy.cmd
+++ azure_deploy.cmd
@@ -15,7 +15,7 @@ IF NOT DEFINED DEPLOYMENT_TARGET (
 )
 
 IF EXIST "%GRADLE_CMD%" (
-  call :ExecuteCmd "%GRADLE_CMD%" clean test war --info
+  call :ExecuteCmd "%GRADLE_CMD%" clean test war --debug
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
