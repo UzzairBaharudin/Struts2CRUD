186c25a8c57f088a9bda3e9337acab92137b4cc4
Chen Yu Pao <windperson@gmail.com>
2014-08-09 08:15:39 +0800

    change back

--- azure_deploy.cmd
+++ azure_deploy.cmd
@@ -14,7 +14,7 @@ IF NOT DEFINED DEPLOYMENT_TARGET (
 )
 
 IF EXIST "%GRADLE_CMD%" (
-  call :ExecuteCmd "%GRADLE_CMD%" test war -S -d
+  call :ExecuteCmd "%GRADLE_CMD%" war
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
